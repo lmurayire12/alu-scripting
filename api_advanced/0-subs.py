1e82369af9fb5074921e066e1b68b06a27f4ec52
lmurayire12 <l.murayire@alustudent.com>
2024-03-02 12:24:07 -0800

    api

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Return number of subscribers for a given subreddit"""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
+    """Return the number of subscribers for the given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-        return response.json().get('data') \
-            .get('subscribers')
+        data = response.json().get('data')
+        if data:
+            return data.get('subscribers', 0)
     return 0
