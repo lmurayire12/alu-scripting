c160a7ce63bf4d4283a4e1f501b3f86f7489acf0
lmurayire12 <l.murayire@alustudent.com>
2024-03-09 01:01:32 -0800

    all tasks done

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
 """ 2-recurse.py """
-import requests
+import praw
 
-
-def recurse(subreddit, hot_list=[], after=None):
-    """ returns list with titles of all hot articles in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after}
-    response = requests.get(
-                                url,
-                                headers=headers,
-                                params=params,
-                                allow_redirects=False
-                            )
-    if response.status_code == 200:
-        data = response.json().get('data')
-        if data is not None:
-            children = data.get('children')
-            if children is not None:
-                for child in children:
-                    hot_list.append(child.get('data').get('title'))
-                after = data.get('after')
-                if after is not None:
-                    return recurse(subreddit, hot_list, after)
-                else:
-                    return hot_list
-        else:
-            return hot_list
-    else:
+def recurse(subreddit, hot_list=None, after=None):
+    if hot_list is None:
+        hot_list = []
+    reddit = praw.Reddit(client_id='YOUR_CLIENT_ID', client_secret='YOUR_CLIENT_SECRET', user_agent='YOUR_USER_AGENT')
+    try:
+        subreddit_info = reddit.subreddit(subreddit)
+    except praw.exceptions.Redirect:
         return None
+    if not subreddit_info.exists():
+        return None
+    hot_posts = subreddit_info.hot(limit=100, after=after)
+    for post in hot_posts:
+        hot_list.append(post.title)
+    if len(hot_posts) == 100:
+        return recurse(subreddit, hot_list, after=hot_posts[-1].name)
+    else:
+        return hot_list
